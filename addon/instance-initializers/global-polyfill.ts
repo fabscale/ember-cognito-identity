04d0b71cea86382f81f62e1237cd9ee22a5e9b94
Francesco Novy <francesco@cropster.com>
2019-10-23 17:06:40 +0200

    feat: Add typing for global-polyfill

--- addon/instance-initializers/global-polyfill.ts
+++ addon/instance-initializers/global-polyfill.ts
@@ -1,7 +1,8 @@
-export function initialize() {
+export function initialize(): void {
   // Some dependencies of amazon-cognito-identity-js (wrongly) rely on `global` to exist
   // So we do a very naive "fix" here to make that work
   if (typeof window.global === 'undefined') {
+    // @ts-ignore
     window.global = window;
   }
 }
