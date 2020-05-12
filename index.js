f5664b3597e2ca72038e4abf4a51e67e251d14fe
Francesco Novy <francesco@fabscale.com>
2020-05-12 08:23:50 +0200

    fix: Remove duplicate lib inclusion
    This was forgotten to remove when we moved to use ember-auto-import.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 'use strict';
-const path = require('path');
-const MergeTrees = require('broccoli-merge-trees');
-const Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: require('./package').name,
@@ -24,30 +21,5 @@ module.exports = {
     }
 
     this._super.included.apply(this, arguments);
-  },
-
-  treeForVendor(vendorTree) {
-    let amazonCognitoIdentityJsPath = this._getPath();
-
-    let trees = [];
-    if (vendorTree) {
-      trees.push(vendorTree);
-    }
-
-    let amazonCognitoIdentityJsTree = new Funnel(amazonCognitoIdentityJsPath, {
-      destDir: 'amazon-cognito-identity-js',
-    });
-
-    trees.push(amazonCognitoIdentityJsTree);
-
-    return new MergeTrees(trees, { overwrite: true });
-  },
-
-  _getPath() {
-    let basePath = path.dirname(require.resolve('amazon-cognito-identity-js'));
-
-    // The path returned here is e.g. node_modules/amazon-cognito-identity-js/lib
-    // We want to move one step out to get the dist folder
-    return basePath.replace('/lib', '/dist').replace('\\lib', '\\dist');
   },
 };
