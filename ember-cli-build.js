fc3ac346df713b104ef26d635320728404fcc9f6
Francesco Novy <francesco@fabscale.com>
2020-09-09 16:16:58 +0200

    chore: Run tests against embroider

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,12 +5,17 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {});
 
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
+  if ('@embroider/webpack' in app.dependencies()) {
+    /* eslint-disable node/no-missing-require */
+    let { Webpack } = require('@embroider/webpack');
+    return require('@embroider/compat').compatBuild(app, Webpack, {
+      staticAddonTestSupportTrees: true,
+      staticAddonTrees: true,
+      staticHelpers: true,
+      staticComponents: true,
+    });
+    /* eslint-enable node/no-missing-require */
+  }
 
   return app.toTree();
 };
