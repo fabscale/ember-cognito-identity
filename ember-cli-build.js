ee6ce5dc7c5ffaf6f95e0eacfd482df9106b0588
Francesco Novy <francesco@fabscale.com>
2020-12-10 10:32:07 +0100

    chore(ci): Fix embroider tests on CI

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,17 +5,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {});
 
-  if ('@embroider/webpack' in app.dependencies()) {
-    /* eslint-disable node/no-missing-require */
-    let { Webpack } = require('@embroider/webpack');
-    return require('@embroider/compat').compatBuild(app, Webpack, {
-      staticAddonTestSupportTrees: true,
-      staticAddonTrees: true,
-      staticHelpers: true,
-      staticComponents: true,
-    });
-    /* eslint-enable node/no-missing-require */
-  }
-
-  return app.toTree();
+  let { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app);
 };
