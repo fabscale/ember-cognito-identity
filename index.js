354c671418f1ff9dc4fecee9765d428cc8702fe4
Francesco Novy <francesco@fabscale.com>
2021-07-01 09:30:40 +0200

    chore: Remove pretender

--- index.js
+++ index.js
@@ -18,14 +18,4 @@ module.exports = {
       },
     },
   },
-
-  included() {
-    // If tests are not running, make sure pretender is not included
-    let app = this._findHost();
-    if (!app.tests) {
-      this.options.autoImport.exclude.push('pretender');
-    }
-
-    this._super.included.apply(this, arguments);
-  },
 };
